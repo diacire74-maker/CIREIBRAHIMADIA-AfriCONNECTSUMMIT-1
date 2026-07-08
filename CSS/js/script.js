5248911b66fb0f7c1b2f02bd15647e3078c73e8a
Ton Nom <tonemail@example.com>
2026-07-08 15:03:31 +0000

    Ajout du Hero et du compte à rebours

--- CSS/js/script.js
+++ CSS/js/script.js
@@ -1,4 +1,25 @@
-// Année dynamique
+// ===========================
+// Compte à rebours
+// ===========================
 
-document.getElementById("year").textContent =
-new Date().getFullYear();
+const eventDate = new Date("November 20, 2026 09:00:00").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance = eventDate - now;
+
+    document.getElementById("days").textContent =
+        Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    document.getElementById("hours").textContent =
+        Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    document.getElementById("minutes").textContent =
+        Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    document.getElementById("seconds").textContent =
+        Math.floor((distance % (1000 * 60)) / 1000);
+
+},1000);
